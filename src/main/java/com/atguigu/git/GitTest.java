83e14948b9c10a85101566c684985c910e27bc15
wanghq363678 <895680442@qq.com>
2023-07-24 14:05:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author wanghq3
+ * @create2023-07-24-13:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
