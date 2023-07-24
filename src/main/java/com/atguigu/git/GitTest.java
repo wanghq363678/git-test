002e7b2bdd132cd096c9e9631a1a2957553e22f0
wanghq363678 <112476027+wanghq363678@users.noreply.github.com>
2023-07-24 16:24:10 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot fixed test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 
 }
